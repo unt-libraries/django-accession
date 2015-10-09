e50dccd39ebe33a77cb670f8d73c52cf0b20ccae
Gio Gottardi <ManuelGGottardi@gmail.com>
2015-10-09 14:07:53 -0500

    Changed databrowse import and added app's URLs to test project.

--- tests/urls.py
+++ tests/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import patterns, include, url
+from accession.admin_views import duplicates
+from accession.site_views import main_admin
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/accession/(?P<model_selected>.*)/duplicates/$', duplicates),
+    url(r'^admin/', include(main_admin.urls)),
+    url(r'^tfc/', include('accession.urls')),
 )
