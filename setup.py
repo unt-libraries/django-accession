5e6aaa01622f6f816a0f7a153b80612c4ee5f011
Gio Gottardi <ManuelGGottardi@gmail.com>
2015-10-12 13:59:10 -0500

    Filled in MANIFEST.in, started filling in setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-accession',
+    version='0.9.0',
+    packages=find_packages(exclude=['tests*']),
+    description='',
+    long_description='See the home page for more information.',
+    include_package_data=True,
+    install_requires=[],
+    url='https://github.com/unt-libraries/django-accession',
+    author='University of North Texas Libraries',
+    author_email='mark.phillips@unt.edu',
+    license='BSD',
+    keywords=[],
+    classifiers=[
+        'Natural Language :: English',
+        'Environment :: Web Environment',
+        'Framework :: Django :: 1.6',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ]
+)
