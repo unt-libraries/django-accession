4f426eb2a23509172030ecf902bb5002b0ea475f
Gio Gottardi <somexpert@gmail.com>
2015-11-18 13:27:53 -0600

    Changed version to 1.0.0, added zip_safe=False, and added missing dependency to setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-accession',
-    version='0.9.0',
+    version='1.0.0',
     packages=find_packages(exclude=['tests*']),
     description='',
     long_description='See the home page for more information.',
     include_package_data=True,
     install_requires=[
         'django-localflavor>=1.0',
+        'django-queryset-csv>=0.3.1',
     ],
+    zip_safe=False,
     url='https://github.com/unt-libraries/django-accession',
     author='University of North Texas Libraries',
     author_email='mark.phillips@unt.edu',
