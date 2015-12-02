e6e392ffcfba8d4b53d15e32d2ce1ae7070e0bce
Gio Gottardi <somexpert@gmail.com>
2015-12-02 13:24:15 -0600

    Made the print view, duplicates view, and csv export views accessible from the test project's admin site.

--- tests/urls.py
+++ tests/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls import patterns, include, url
-from accession.admin_views import duplicates
+from django.contrib import admin
+
+from accession.admin_views import duplicates, export_csv, print_view
 from accession.admin import main_admin
 
-from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^admin/accession/(?P<model_selected>.*)/duplicates/$', duplicates),
+    url(r'^admin/(accession)/([^/]+)/([0-9]+)/print/$', print_view),
+    url(r'^admin/(accession)/(?P<model_selected>.*)/duplicates/$', duplicates),
+    url(r'^admin/(accession)/([^/]+)/export_csv/$', export_csv),
     url(r'^admin/', include(main_admin.urls)),
     url(r'^tfc/', include('accession.urls')),
 )
