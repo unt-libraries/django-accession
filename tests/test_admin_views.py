c5122430abcd5952ced6fffebde5ef1ef36fefa7
Gio Gottardi <somexpert@gmail.com>
2015-12-07 11:40:25 -0600

    Removed all tests related to duplicates view and removed url from test project.

--- tests/test_admin_views.py
+++ tests/test_admin_views.py
@@ -9,52 +9,6 @@ from .factories import DonorFactory, AccessionFactory
 pytestmark = pytest.mark.django_db
 
 
-class TestDuplicates():
-
-    def test_returns_200_with_existing_model(self, rf, admin_user):
-        request = rf.get('/')
-        request.user = admin_user
-
-        response = admin_views.duplicates(request, 'donor')
-
-        assert response.status_code == 200
-
-    def test_raises_404_when_model_does_not_exist(self, rf, admin_user):
-        request = rf.get('/')
-        request.user = admin_user
-
-        with pytest.raises(Http404):
-            admin_views.duplicates(request, 'dne')
-
-    def test_template_used(self, admin_client):
-        response = admin_client.get('/admin/accession/donor/duplicates/')
-        assert response.templates[0].name == 'admin/accession/duplicates.html'
-
-    def test_with_duplicates(self, rf, admin_user):
-        # Non-duplicate entry
-        DonorFactory(last_name='Libman')
-        # Duplicate entries
-        DonorFactory.create_batch(4, last_name='Smith')
-
-        request = rf.get('/')
-        request.user = admin_user
-
-        response = admin_views.duplicates(request, 'donor')
-
-        assert 'Libman' not in response.content
-        assert 'Smith' in response.content
-
-    def test_without_duplicates(self, rf, admin_user):
-        DonorFactory.create_batch(5)
-
-        request = rf.get('/')
-        request.user = admin_user
-
-        response = admin_views.duplicates(request, 'donor')
-
-        assert 'There are no duplicate entries' in response.content
-
-
 class TestPrintView():
 
     def test_returns_200(self, rf, admin_user):
--- tests/urls.py
+++ tests/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from accession.admin_views import duplicates, export_csv, print_view
+from accession.admin_views import export_csv, print_view
 from accession.admin import main_admin
 
 admin.autodiscover()
@@ -9,7 +9,6 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     url(r'^admin/(accession)/([^/]+)/([0-9]+)/print/$', print_view),
-    url(r'^admin/(accession)/(?P<model_selected>.*)/duplicates/$', duplicates),
     url(r'^admin/(accession)/([^/]+)/export_csv/$', export_csv),
     url(r'^admin/', include(main_admin.urls)),
     url(r'^tfc/', include('accession.urls')),
