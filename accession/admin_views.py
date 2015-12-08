a0dcd82e89208c1ca608762260d7f03dfa492a80
Gio Gottardi <somexpert@gmail.com>
2015-12-08 14:51:24 -0600

    Fixed the csv export of foreign key fields so they show more logical data.

--- accession/admin_views.py
+++ accession/admin_views.py
@@ -7,6 +7,7 @@ from django.shortcuts import render
 from django.contrib.admin.views.decorators import staff_member_required
 
 from accession.admin import accession_admin
+from accession.models import Accession, Donor, Object
 
 
 @staff_member_required
@@ -80,4 +81,60 @@ def export_csv(request, app, model):
     # Using one of Django's private API's (_registry). May break with updates.
     model_admin = accession_admin._registry[model]
     results, _ = model_admin.get_search_results(request, objects_list, q)
-    return render_to_csv_response(results)
+
+    # Modify the querysets to show something more meaningful than object IDs.
+    field_serializer_map = {}
+    field_header_map = {}
+
+    if model == Accession:
+        field_serializer_map = {'donor_id': (lambda x: str(Donor.objects.get(id=x)))}
+        field_header_map = {'donor_id': 'donor'}
+
+    elif model == Donor:
+        fields_to_show = [
+            'id', 'salutation', 'first_name', 'middle_name', 'last_name',
+            'organization_name', 'donor_type', 'gender', 'address_1',
+            'address_2', 'city__city', 'state', 'postal_code',
+            'country__country', 'phone_number_1', 'phone_number_2',
+            'email_address', 'comments'
+        ]
+
+        field_header_map = {'city__city': 'city', 'country__country': 'country'}
+
+        results = results.values(*fields_to_show)
+
+    elif model == Object:
+        fields_to_show = [
+            'id', 'object_number', 'accession_number__accession_number',
+            'object_description', 'related_objects', 'original_numbers',
+            'date_object_creation', 'object_era', 'remarks', 'location_remarks',
+            'construction', 'exhibitions', 'publications', 'provenance',
+            'condition_statement', 'price', 'public_notes',
+            'designer__designer', 'label__label', 'retailer__retailer_name',
+            'retailer_label__retailer_label', 'classification__classification',
+            'country__country', 'gender', 'location__location',
+            'condition__condition', 'material__material',
+            'measurement__measurement', 'type__object_type', 'parts__part',
+            'date_record_added', 'date_record_last_edited'
+        ]
+
+        field_header_map = {
+            'accession_number__accession_number': 'accession number',
+            'designer__designer': 'designer',
+            'label__label': 'label',
+            'retailer__retailer_name': 'retailer',
+            'retailer_label__retailer_label': 'retailer label',
+            'classification__classification': 'classification',
+            'country__country': 'country',
+            'location__location': 'location',
+            'condition__condition': 'condition',
+            'material__material': 'material',
+            'measurement__measurement': 'measurement',
+            'type__object_type': 'type',
+            'parts__part': 'parts'
+        }
+
+        results = results.values(*fields_to_show)
+
+    return render_to_csv_response(results, field_header_map=field_header_map,
+                                  field_serializer_map=field_serializer_map)
