98c577832ee88c11fe8a404a89cd9988c3efa6f0
Gio Gottardi <somexpert@gmail.com>
2016-01-19 09:55:22 -0600

    Fixed main admin site registration.

--- tests/urls.py
+++ tests/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from accession.admin_views import export_csv, print_view
-from accession.admin import main_admin
 
 admin.autodiscover()
 
@@ -10,6 +9,6 @@ urlpatterns = patterns(
     '',
     url(r'^admin/(accession)/([^/]+)/([0-9]+)/print/$', print_view),
     url(r'^admin/(accession)/([^/]+)/export_csv/$', export_csv),
-    url(r'^admin/', include(main_admin.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^tfc/', include('accession.urls')),
 )
